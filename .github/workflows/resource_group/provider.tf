5d5deab3d7c0ba98d3f3f86e719db6d3e5feb458
Anurag Chauhan <send2anurag.chauhan@gmail.com>
2026-07-13 15:44:56 +0530

    run2

---
+++ .github/workflows/resource_group/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  subscription_id = "5162d39b-a4ed-44f0-91aa-9884962cf062"
+  features {}
+}
